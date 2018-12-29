227165d08d11c1afb20d978b3c242480a980e971
SergProgMan <sembratyev@gmail.com>
2018-12-29 21:01:13 +0200

    made submit form

--- app/Http/Controllers/BookmarksController.php
+++ app/Http/Controllers/BookmarksController.php
@@ -9,4 +9,11 @@ class BookmarksController extends Controller
     public function index(){
         return view('home');
     }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required',
+            'url' => 'required',
+        ]);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,16 +8,51 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
+                    <!-- @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
-                    @endif
+                    @endif -->
 
-                    You are logged in!
+                    @include('inc.messages')
+
+                    <button type="button" 
+                            class="btn btn-primary btn-lg" 
+                            data-toggle="modal" 
+                            data-target="#addModal" 
+                            name="button">Add Bookmark</button>
                 </div>
             </div>
         </div>
     </div>
+</div>
+
+<div class="modal" tabindex="-1" role="dialog" id="addModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Add bookmark </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="{{ route('bookmarks.store') }}" method="post">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label>Bookmark Name </label>
+            <input type="text" class="form-control" name="name">
+        </div>
+        <div class="form-group">
+            <label>Bookmark URL</label>
+            <input type="text" class="form-control" name="url">
+      </div>
+      <div class="form-group">
+        <label>Website Description</label>
+        <textarea class="form-control" name="description"></textarea>
+      </div>
+      <input type="submit" name="submit" value="Submit" class="btn btn-success">
+    </div>
+  </div>
 </div>
 @endsection
